58349d4c7d0e64b07e9ffe63a692b9c474331a2c
mlem <159339398+ml3m@users.noreply.github.com>
2024-12-15 16:56:29 +0200

    created the vector class of 3d vector.

---
+++ vec3.h
@@ -0,0 +1,17 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+class vec3 {
+    public:
+        vec3() : e{0,0,0} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
+
+    public:
+        double e[3];
+};
+
+#endif
