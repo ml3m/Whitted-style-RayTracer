a4c48ad62f8af145b8e6f6c2864af4cfd024c0ca
mlem <159339398+ml3m@users.noreply.github.com>
2024-12-15 18:04:03 +0200

    background

---
+++ background.cpp
@@ -0,0 +1,41 @@
+
+#include "color.h"
+#include "vec3.h"
+#include <iostream>
+int main () {
+    
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 16;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0,0,0);
+    auto horizontal = vec3(viewport_width, 0,0);
+    auto vertical = vec3(0,viewport_height,0);
+    auto lower_left_corner = origin - horizontal/2  - vertical/2 - vec3(0,0,focal_length);
+
+    // Render
+    //
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "Scanlines remaining: " << j << '\n';
+        for (int i = 0; i < image_width; ++i) {
+
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            vec3 vec(lower_left_corner + u*horizontal + v*vertical - origin);
+
+            // this creates a "color" variable called pixel_color
+            color pixel_color = getColor(vec);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
--- vec3.h
+++ vec3.h
@@ -10,12 +10,12 @@ using std::sqrt;
 class vec3 {
     public:
         // Default constructor init the vector to (0, 0, 0)
-        vec3() : e{0,0,0} {}
-
-        // Constructor init the vector with specified 
-        // x, y, z components
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
-
+        vec3() { e[0] = 0; e[1] = 0; e[2] = 0; } // Default constructor
+        vec3(double e0, double e1, double e2) { // Parameterized constructor
+            e[0] = e0; 
+            e[1] = e1; 
+            e[2] = e2; 
+        }
         double x() const { return e[0]; } // Get the x-component
         double y() const { return e[1]; } // Get the y-component
         double z() const { return e[2]; } // Get the z-component
